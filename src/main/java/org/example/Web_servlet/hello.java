e896bceb0c2950689560ec65ea494d932ea97430
hyujikoh <dvum0045@gmail.com>
2022-07-19 16:12:18 +0900

    0719 날짜 servlet 테스트 확인

--- src/main/java/org/example/Web_servlet/hello.java
+++ src/main/java/org/example/Web_servlet/hello.java
@@ -1,11 +1,24 @@
 package org.example.Web_servlet;
 
-import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;// 내가 가는데 이미 있다라는것
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+@WebServlet("/hello")
 public class hello extends HttpServlet {
-    public String sayHelloWorldFrom(String from) {
-        String result = "Hello, world, from " + from;
-        System.out.println(result);
-        return result;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 들어오는 파라미터를 UTF-8로 해석
+        req.setCharacterEncoding("UTF-8");
+        // 서블릿이 HTML 파일을 만들 때 UTF-8 로 쓰기
+        resp.setContentType("UTF-8");
+
+        // HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+        resp.setContentType("text/html; charset=utf-8");
+
+        resp.getWriter().append("안녕하세요!");
     }
 }
