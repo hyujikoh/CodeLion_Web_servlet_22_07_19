17a06245e38b93a7092972b74231012370e06541
hyujikoh <dvum0045@gmail.com>
2022-07-21 15:15:05 +0900

    model2 버전 생성

--- src/main/java/org/example/Web_servlet/dan_servlet.java
+++ src/main/java/org/example/Web_servlet/dan_servlet.java
@@ -1,5 +1,6 @@
 package org.example.Web_servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,22 +8,26 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
-@WebServlet("/dan_rq")
-public class dan_Rq extends HttpServlet {
+@WebServlet("/dan_servlet")
+public class dan_servlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Rq rq = new Rq(req, resp);
         // 들어오는 파라미터를 UTF-8로 해석
-;
+        ;
         int dan = rq.getIntParam("dan", 0);
 
         int limit = rq.getIntParam("limit", 0);
 
-        rq.appendBody("<h1>%d단</h1>\n".formatted(dan) );
+        // request에 정보를 담는다.
+        // 왜냐하면 나중에 gugudan2.jsp에서 해당 내용을 꺼낼 수 있기 때문에
+        req.setAttribute("dan", dan);
+        req.setAttribute("limit", limit);
 
-        for (int i = 1; i <= limit; i++) {
-            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
-        }
+        // gugudan2.jsp 에게 나머지 작업을 토스
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/dan2.jsp");
+        requestDispatcher.forward(req, resp);
+
+
     }
 }
