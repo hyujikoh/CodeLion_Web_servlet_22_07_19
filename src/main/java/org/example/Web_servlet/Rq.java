b94b625afee794d21628f194041ae10270361b54
hyujikoh <dvum0045@gmail.com>
2022-07-21 13:40:35 +0900

    Rq로 분리 해서 작성

--- src/main/java/org/example/Web_servlet/Rq.java
+++ src/main/java/org/example/Web_servlet/Rq.java
@@ -9,13 +9,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class Rq {
     HttpServletRequest req;
     HttpServletResponse resp;
-    public Rq(HttpServletRequest req, HttpServletResponse resp) {
+    public Rq(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
         this.req = req;
         this.resp = resp;
+        req.setCharacterEncoding("UTF-8");
+        // 서블릿이 HTML 파일을 만들 때 UTF-8 로 쓰기
+        resp.setContentType("UTF-8");
+
+        // HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+        resp.setContentType("text/html; charset=utf-8");
     }
 
     public int getIntParam(String value, int i) {
--- src/main/java/org/example/Web_servlet/dan_Rq.java
+++ src/main/java/org/example/Web_servlet/dan_Rq.java
@@ -14,12 +14,7 @@ public class dan_Rq extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Rq rq = new Rq(req, resp);
         // 들어오는 파라미터를 UTF-8로 해석
-        req.setCharacterEncoding("UTF-8");
-        // 서블릿이 HTML 파일을 만들 때 UTF-8 로 쓰기
-        resp.setContentType("UTF-8");
-
-        // HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
-        resp.setContentType("text/html; charset=utf-8");
+;
         int dan = rq.getIntParam("dan", 0);
 
         System.out.println(dan);
